dd0cdea2169caa550c2876904effd1da73853f88
svhludnev <32407986+svhludnev@users.noreply.github.com>
2023-06-13 07:32:43 +0300

    Add files via upload

---
+++ TESTWEB.ps1
@@ -0,0 +1,17 @@
+﻿
+
+while ($true) {
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://docs-test.zenit.ru/default.aspx
+
+$JSONInfo =  $ie.Content
+#$JSONInfo = ConvertTo-Xml -InputObject  $JSONInfo.result
+
+$iewall= Invoke-WebRequest -Uri'http://docs-test.zenit.ru/_layouts/15/DT.Orgstructure/OrgstructurePage.aspx' -Method Get -ContentType "text/plain; charset=utf-8"
+$JSONInfoWall = $iewall
+$JSONInfoWall
+
+sleep 100
+
+}
